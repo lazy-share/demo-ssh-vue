d68b47045e53469631386e31486a81c91b46d1c5
lzy369 <1059446341@qq.com>
2020-02-05 20:08:39 +0800

    init server-api project

---
+++ server-api/src/main/java/com/lazy/demo/demospringbootspringdatavue/DemoSpringbootSpringdataVueApplication.java
@@ -0,0 +1,13 @@
+package com.lazy.demo.demospringbootspringdatavue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootSpringdataVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootSpringdataVueApplication.class, args);
+    }
+
+}
---
+++ server-api/src/test/java/com/lazy/demo/demospringbootspringdatavue/DemoSpringbootSpringdataVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazy.demo.demospringbootspringdatavue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootSpringdataVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
